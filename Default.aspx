28a959214ee7d34e8d2b7c19126cdad30abeb438
naseem <naseem@NASEEMC1B1>
2020-09-25 17:37:52 +0300

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="LoginWithMSSql.Default" %>
+
+<!DOCTYPE html>
+
+
+<!-- https://www.youtube.com/watch?v=yBsl84hDtUg -->
+
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        Full Name
+                    </td>
+                    <td>
+                        <asp:TextBox ID="fullname_tv" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Username
+                    </td>
+                    <td>
+                        <asp:TextBox ID="username_tv" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Password
+                    </td>
+                    <td>
+                        <asp:TextBox ID="password_tv" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <asp:Label id="results" runat="server" Text="" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <asp:Button id="Button1" runat="server" Text="SignUp new user" OnClick="Button1_Click"/>
+                        <asp:Button id="Button2" runat="server" Text="Login" OnClick="Button2_Click"/>
+                    </td>
+                </tr>
+            </table>
+            
+            <br />
+
+            <asp:GridView id="grid" runat="server"></asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LoginWithMSSql
+{
+
+    public partial class Default : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\WebDB.mdf;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+
+            con.Open();
+
+            DisplayData();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            //cmd.CommandText = "insert into users ('admin','admin','admin')";
+            cmd.CommandText = "insert into users values('" + username_tv.Text + "','" + password_tv.Text + "','" + fullname_tv.Text + "')";
+            cmd.ExecuteNonQuery();
+
+            username_tv.Text = "";
+            password_tv.Text = "";
+            fullname_tv.Text = "";
+
+            cmd.Dispose();
+
+            DisplayData();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from users where username='"+username_tv.Text+ " and 'password='" + password_tv.Text + "'";
+            SqlDataReader sqlr = cmd.ExecuteReader();
+            if (sqlr.Read())
+            {
+                results.Text = "Login Succeded!";
+            } else
+            {
+                results.Text = "Login failed!";
+            }
+
+            sqlr.Close();
+            cmd.Dispose();
+
+        }
+
+        public void DisplayData()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from users";
+            cmd.ExecuteNonQuery();
+
+            DataTable data = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(data);
+            grid.DataSource = data;
+            grid.DataBind();
+            cmd.Dispose();
+
+        }
+
+    }
+}
