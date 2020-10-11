8ee2305e0c518a128ecaa07aff4a4f5c791694bf
Naseem <naseem@movez.ai>
2020-10-11 22:05:43 +0300

    fix view crime

--- ViewCrime.aspx.cs
+++ ViewCrime.aspx.cs
@@ -9,12 +9,27 @@ using System.Data.SqlClient;
 
 namespace CrimeManagement
 {
-    public partial class ViewCrime : System.Web.UI.Page
+    public partial class ViewCrime : BaseDataPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            this.TextBox1.Text = Request.QueryString[0];
+            base.Page_Load(sender, e);
 
+            if (!String.IsNullOrEmpty(Request.QueryString["id"]))
+            {
+                this.TextBox1.Text = Request.QueryString["id"];
+                Crime crime = GetCrime(TextBox1.Text);
+                if (crime != null)
+                {
+                    TextBox2.Text = crime.description;
+                } else
+                {
+                    // TODO crime not found
+                }
+            } else
+            {
+                // TODO bad params, show error
+            }
         }
     }
 }
