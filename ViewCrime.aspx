c0a7eda3fe3eb908a4419ce1f4f2dd8e25f47796
Naseem <naseem@movez.ai>
2020-11-07 14:11:13 +0200

    add integration with cime suspects screen

--- ViewCrime.aspx
+++ ViewCrime.aspx
@@ -12,34 +12,77 @@
     </style>
 </head>
 <body>
-    <form id="form1" runat="server">
-        <div class="auto-style1">
-            <br />
-            <asp:Label ID="Label1" runat="server" Text="Crime ID :"></asp:Label>
+<form id="form1" runat="server">
+    <div class="auto-style1">
+        <br/>
+        <asp:Label ID="Label1" runat="server" Text="Crime ID :"></asp:Label>
         &nbsp;<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
-            <br />
-            <asp:Label ID="Label2" runat="server" Text="Details :"></asp:Label>
-            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
-            <br />
-            &nbsp;<asp:Label ID="Label3" runat="server" Text="Place :"></asp:Label>
-&nbsp;
-            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
-            <br />
-            <br />
-            <br />
+        <br/>
+        <asp:Label ID="Label2" runat="server" Text="Details :"></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br/>
+        &nbsp;<asp:Label ID="Label3" runat="server" Text="Place :"></asp:Label>
+        &nbsp;
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br/>
+        <br/>
+        <br/>
 
-            <br />
-            <br />
-            <asp:GridView ID="GridView1" runat="server">
-                <Columns>
-                    <asp:TemplateField HeaderText="Delete">
-                        <ItemTemplate>
-                            <asp:Button ID="ButDelete" runat="server" Text="Delete" />
-                        </ItemTemplate>
-                    </asp:TemplateField>
-                </Columns>
-            </asp:GridView>
-        </div>
-    </form>
+        <br/>
+        <br/>
+        <asp:GridView ID="GridView1" runat="server">
+            <Columns>
+                <asp:TemplateField HeaderText="Delete">
+                    <ItemTemplate>
+                        <asp:Button ID="ButDelete" runat="server" Text="Delete"/>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+        <br/>
+        <br/>
+
+
+        <br/>
+
+                <%-- Add suspects--%>
+        
+        <asp:TextBox ID="FilterSusbectTB" runat="server" OnTextChanged="FilterSusbectTB_TextChanged" placeholder="Search suspect by name"
+                     AutoPostBack="true"/>
+        <asp:GridView ID="GridViewAddSuspects" runat="server" OnRowCommand="Suspects_RowCommand">
+            <Columns>
+                <asp:TemplateField HeaderText="Add Suspect">
+                    <ItemTemplate>
+                        <asp:Button ID="ButAddSuspect" runat="server" Text="Add" CommandName="AddSuspect" CommandArgument='<%# Eval("ID") %>'/>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+
+        <br/>
+        <%-- Show suspects--%>
+        <br/>
+
+        <asp:GridView ID="GridViewSuspects" runat="server" OnRowCommand="Suspects_RowCommand">
+            <Columns>
+                <asp:TemplateField HeaderText="Delete Suspect">
+                    <ItemTemplate>
+                        <asp:Button ID="ButDeleteSuspect" runat="server" Text="Delete" CommandName="DeleteSuspect" CommandArgument='<%# Eval("crimeId") + "," + Eval("SuspectsID") %>'/>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="ChangeCommitStatus">
+                    <ItemTemplate>
+                        <asp:Button ID="ChangeCommitStatus" runat="server" Text='<%# int.Parse(Eval("is_committed").ToString()) == 0 ? "Change to  committed" : " Change to uncommitted"  %>' CommandName="ChangeCommitStatus" CommandArgument='<%# Eval("crimeId") + "," + Eval("SuspectsID") + "," + Eval("is_committed") %>'/>
+                        
+                        <%--
+                        <asp:CheckBox ID="ChangeCommitStatusc" runat="server" Checked='<%# int.Parse(Eval("is_committed").ToString()) == 0 ? false : true  %>' runat="server" OnCheckedChanged="chkApproved_CheckedChanged" AutoPostBack="true"  Text="IsCommited" CommandName="ChangeCommitStatus" CommandArgument='<%# Eval("crimeId") + "," + Eval("SuspectsID") + "," + Eval("is_committed") %>'/>
+                        --%>
+                    
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+</form>
 </body>
 </html>
--- ViewCrime.aspx.cs
+++ ViewCrime.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 
 namespace CrimeManagement
@@ -14,27 +15,27 @@ namespace CrimeManagement
         protected void Page_Load(object sender, EventArgs e)
         {
             base.Page_Load(sender, e);
+            if (!Page.IsPostBack) {
 
-            if (!String.IsNullOrEmpty(Request.QueryString["id"]))
-            {
-                this.TextBox1.Text = Request.QueryString["id"];
-                Crime crime = GetCrime(TextBox1.Text);
-                if (crime != null)
-                {
-                    TextBox2.Text = crime.description;
-                    TextBox3.Text = crime.location;
-                    //TextBox4.Text = crime.time;
-                    //TextBox5.Text = crime.date;
-                    ShowWitness(Request.QueryString["id"]);
+                if (!String.IsNullOrEmpty(Request.QueryString["id"])) {
+                    this.TextBox1.Text = Request.QueryString["id"];
+                    Crime crime = GetCrime(TextBox1.Text);
+                    if (crime != null) {
+                        TextBox2.Text = crime.description;
+                        TextBox3.Text = crime.location;
+                        //TextBox4.Text = crime.time;
+                        //TextBox5.Text = crime.date;
+                        ShowWitness(Request.QueryString["id"]);
+                        ReloadSuspects();
+                    }
+                    else {
+                        // TODO crime not found
+                    }
                 }
-                else
-                {
-                    // TODO crime not found
+                else {
+                    // TODO bad params, show error
                 }
-            }
-            else
-            {
-                // TODO bad params, show error
+
             }
         }
 
@@ -49,7 +50,67 @@ namespace CrimeManagement
             cmd.Dispose();
         }
 
+        protected void Suspects_RowCommand(Object sender, GridViewCommandEventArgs e) {
+            if (e.CommandName == "ChangeCommitStatus") {
+                string crimeID = e.CommandArgument.ToString().Split(',')[0];
+                string suspectId = e.CommandArgument.ToString().Split(',')[1];
+                bool isCommitted = (e.CommandArgument.ToString().Split(',')[2] != "0");
+                ChangeSuspectCommittedCrime(crimeID,suspectId, !isCommitted);
+                ReloadSuspects();
+            }
+            if (e.CommandName == "DeleteSuspect") {
+                string crimeID = e.CommandArgument.ToString().Split(',')[0];
+                string suspectId = e.CommandArgument.ToString().Split(',')[1];
+                DeleteSuspectFromCrime(crimeID,suspectId);
+                ReloadSuspects();
+            }
+            if (e.CommandName == "AddSuspect") {
+                string crimeID = Request.QueryString["id"];
+                string suspectId = e.CommandArgument.ToString();
+                AddSuspectToCrime(crimeID, suspectId);
+                FilterSusbectTB.Text = null;
+                GridViewAddSuspects.Visible = false;
+                ReloadSuspects();
+            }
+        }
+        protected void chkApproved_CheckedChanged(object sender, EventArgs e)
+        {
+            // https://stackoverflow.com/questions/38184888/get-a-checkbox-to-command-a-gridview-row
+            // CheckBox chkApproved = (CheckBox)sender;
+            // GridViewRow gridViewRow = (GridViewRow)chkApproved.Parent.Parent;
+            // int orderID = (int)GridViewdoc.DataKeys[gridViewRow.RowIndex].Value;
+            // bool approved = chkApproved.Checked;
+            
+            //Your update method
+            // ChangeSuspectCommittedCrime(crimeId,sus approved);
+            //Your data load method
+            ReloadSuspects();
+        }
         
+        protected void FilterSusbectTB_TextChanged(object sender, EventArgs e) {
+            SqlCommand cmd = QueryData("Suspects", "SuspectName LIKE '%" + FilterSusbectTB.Text +"%'");
+            DataTable data = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(data);
+            GridViewAddSuspects.DataSource = data;
+            GridViewAddSuspects.DataBind();
+            cmd.Dispose();
+        }
+
+        private void ReloadSuspects() {
+            string table =
+                "crimes JOIN CrimeSuspects ON crimes.Id = CrimeSuspects.crimeId  JOIN Suspects ON Suspects.Id = CrimeSuspects.SuspectsID";
+            
+            SqlCommand cmd= QueryData(table, "crimes.ID=" + Request.QueryString["id"] );
+
+            DataTable data = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(data);
+            GridViewSuspects.DataSource = data;
+            GridViewSuspects.DataBind();
+            cmd.Dispose();
+            
+        }
     }
 
         
